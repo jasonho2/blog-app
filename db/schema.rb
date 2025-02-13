45d9547fd27ecb825f36264c8fdc370e0c09ee44
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-02-13 12:00:25 -0800

    Add second model for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_181013) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_13_195155) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
